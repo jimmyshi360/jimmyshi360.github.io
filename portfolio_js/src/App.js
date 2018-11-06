a9f33fcc647abf0763de3db49c33a77fd885c5c1
Jimmy Shi <jimmyshi360@gmail.com>
2018-11-05 21:27:57 -0500

    got props working

--- portfolio_js/src/App.js
+++ portfolio_js/src/App.js
@@ -6,12 +6,13 @@ import Portfolio from './components/portfolio/portfolio';
 import Testimonials from  './components/testimonials/testimonials';
 import ContactUs from './components/contactus/contactus';
 import Footer from './components/footer/footer';
+import resumeData from './resumeData';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header resumeData={resumeData}/>
         <About />
         <Resume />
         <Portfolio />
---
+++ portfolio_js/src/resumeData.js
@@ -0,0 +1,6 @@
+let resumeData = {
+  "imagebaseurl":"http://localhost:3000/",
+  "name": "Jimmy Shi",
+  "role": "Frontend Developer"
+  }
+export default resumeData
