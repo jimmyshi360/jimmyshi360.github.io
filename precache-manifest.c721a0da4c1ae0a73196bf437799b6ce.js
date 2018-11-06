5b194160d93de11d4e262f48aa6adae45292f93e
Jimmy Shi <jimmyshi360@gmail.com>
2018-11-05 21:36:00 -0500

    first build deployed

---
+++ precache-manifest.c721a0da4c1ae0a73196bf437799b6ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8c781fd4aafa66dd0a8",
+    "url": "/static/js/main.a8c781fd.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "a8c781fd4aafa66dd0a8",
+    "url": "/static/css/main.c6929053.chunk.css"
+  },
+  {
+    "revision": "bf1f4472c90e13b22c233a2280345b3d",
+    "url": "/index.html"
+  }
+];
--- src/resumeData.js
+++
@@ -1,6 +0,0 @@
-let resumeData = {
-  "imagebaseurl":"https://jimmyshi.github.io/",
-  "name": "Jimmy Shi",
-  "role": "Frontend Developer"
-  }
-export default resumeData
